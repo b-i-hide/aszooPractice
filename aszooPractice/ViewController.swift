b0a4893a47b492705fedf2964a030de64b165488
b-i-hide <b.i1993patriots@gmail.com>
2017-03-26 15:19:07 +0900

    Initial Commit

---
+++ aszooPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aszooPractice
+//
+//  Created by 大島秀顕 on 2017/03/26.
+//  Copyright © 2017年 hideaki oshima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
